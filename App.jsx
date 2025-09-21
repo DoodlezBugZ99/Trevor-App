f2a1b0358cb83553112cbade04f68db4844c6fa2
DoodlezBugZ99 <amanda.martintully@gmail.com>
2025-09-21 02:59:25 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
 
