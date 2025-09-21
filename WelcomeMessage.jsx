1567b6a44068f2e1ca3137ba14965be9fc4a9d0e
DoodlezBugZ99 <amanda.martintully@gmail.com>
2025-09-21 02:58:18 -0700

    Add files via upload

---
+++ WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const WelcomeMessage = () => {
+  return (
+    <div className="p-4 bg-purple-100 rounded-xl shadow-md text-center text-xl font-bold text-purple-800">
+      Welcome, dO0dLeBugz19!
+    </div>
+  );
+};
+
+export default WelcomeMessage;
